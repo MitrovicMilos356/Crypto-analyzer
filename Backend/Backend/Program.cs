3335afb5c3ef1ade1f144848b1a2cab42ed4112c
MitrovicMilos356 <mickomitro01@gmail.com>
2025-03-11 14:16:08 +0100

    Connected front and back through API

---
+++ Backend/Backend/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.WithOrigins("http://localhost:3000") // Allow React frontend
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+var app = builder.Build();
+app.UseCors("AllowReactApp"); // Apply CORS policy
+
+app.MapGet("/api/crypto", () => new { message = "Crypto API Running" });
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
