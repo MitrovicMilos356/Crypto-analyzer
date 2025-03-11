f15690dd7611cf9a5fb92858c9f3e66155542f0f
MitrovicMilos356 <mickomitro01@gmail.com>
2025-03-11 14:03:29 +0100

    Connected front and back through API

--- Frontend/crypto-analyzer/src/App.js
+++ Frontend/crypto-analyzer/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getCryptoData } from "./api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cryptoData, setCryptoData] = useState(null);
+
+    useEffect(() => {
+        getCryptoData().then(data => setCryptoData(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Crypto App</h1>
+            {cryptoData ? <p>{cryptoData.message}</p> : <p>Loading...</p>}
+        </div>
+    );
 }
 
 export default App;
